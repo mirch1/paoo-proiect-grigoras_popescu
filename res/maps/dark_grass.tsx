c185b1885f3af74bcfdf33e874c43dacbd0c7a92
Daria36774 <dariagrigoras10@gmail.com>
2026-05-10 13:42:33 +0300

    Add files via upload

---
+++ res/maps/dark_grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="dark_grass" tilewidth="48" tileheight="48" tilecount="12" columns="3">
+ <image source="../textures/dark_grass_tiles_48x48.png" width="144" height="192"/>
+</tileset>
---
+++ res/maps/dark_road_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="dark_road_tiles" tilewidth="48" tileheight="48" tilecount="676" columns="26">
+ <image source="../textures/dark_road_tiles.png" width="1254" height="1254"/>
+</tileset>

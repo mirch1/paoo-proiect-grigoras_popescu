35355810adc46882b5cedba36dd454029b62cff0
Daria36774 <dariagrigoras10@gmail.com>
2026-05-12 14:03:32 +0300

    Add files via upload

---
+++ res/maps/great_hall_tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="great_hall_tileset2" tilewidth="48" tileheight="48" tilecount="672" columns="32">
+ <image source="../../../../../../Downloads/great_hall_tileset2.png" width="1536" height="1024"/>
+</tileset>

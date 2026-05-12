f4191b653ae38a68efad295fb22fba0e7f1a1239
Daria36774 <dariagrigoras10@gmail.com>
2026-05-12 08:48:46 +0300

    Add files via upload

---
+++ res/maps/Dungeon_Tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Dungeon_Tileset2" tilewidth="48" tileheight="48" tilecount="561" columns="33">
+ <image source="../../../../Castle-Dungeon_Tiles/Castle-Dungeon_Tiles/Tileset.png" width="1600" height="832"/>
+</tileset>

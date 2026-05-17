3ec4826852b1c855aa1ad9949cb9977c0d00f04d
Daria36774 <dariagrigoras10@gmail.com>
2026-05-17 09:23:46 +0300

    Enhance image loading with exception handling
    Added error handling for image loading and refactored method.

--- src/PaooGame/Graphics/ImageLoader.java
+++ src/PaooGame/Graphics/ImageLoader.java
@@ -4,7 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import PaooGame.Exceptions.ResourceLoadException;
 import static java.lang.System.exit;
 
 /*! \class public class ImageLoader
@@ -12,28 +12,23 @@ import static java.lang.System.exit;
  */
 public class ImageLoader
 {
-    /*! \fn  public static BufferedImage loadImage(String path)
-        \brief Incarca o imagine intr-un obiect BufferedImage si returneaza o referinta catre acesta.
+    public static BufferedImage LoadImage(String path) {
+        try {
+            BufferedImage image = ImageIO.read(ImageLoader.class.getResource(path));
 
-        \param path Calea relativa pentru localizarea fisierul imagine.
-     */
-    public static BufferedImage LoadImage(String path)
-    {
-            /// Avand in vedere exista situatii in care fisierul sursa sa nu poate fi accesat
-            /// metoda read() arunca o excpetie ce trebuie tratata
-        try
-        {
-                /// Clasa ImageIO contine o serie de metode statice pentru file IO.
-                /// Metoda read() are ca argument un InputStream construit avand ca referinta
-                /// directorul res, director declarat ca director de resurse in care se gasesc resursele
-                /// proiectului sub forma de fisiere sursa.
-            return ImageIO.read(ImageLoader.class.getResource(path));
+            if (image == null) {
+                throw new ResourceLoadException("Imaginea nu a putut fi incarcata: " + path);
+            }
+
+            return image;
+
+        } catch (ResourceLoadException e) {
+            throw e;
+
+        } catch (Exception e) {
+            throw new ResourceLoadException("Eroare la incarcarea imaginii: " + path, e);
         }
-        catch(IOException e)
-        {
-                /// Afiseaza informatiile necesare depanarii.
-            e.printStackTrace();
-        }
-        return null;
-    }}
+    }
+}
+
 
